8626f2508e61316e38742f8ecefc6318e9325214
Kim Jangwook <kim.jangwook.1029@gmail.com>
2021-02-21 13:39:57 +0900

    Send Mail

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [ \App\Http\Controllers\ProfileController::class, 'welcome' ]);
+Route::get('/test', [ \App\Http\Controllers\ProfileController::class, 'test' ]);
+Route::post('/contact', [ \App\Http\Controllers\ProfileController::class, 'contact' ]);
